07f11f24b6e42907cf3eddbd5108fc2c5c9da7e7
esupav <esuvavnnn@gmail.com>
2025-04-06 04:04:26 -0700

    smallest num

---
+++ smallest number.cpp
@@ -0,0 +1,16 @@
+
+// c++ program to find the smallest number in an array 
+#include<iostream>
+using namespace std;
+    int main(){
+      int gec[]={7,13,5,6,8,9}; //arr=gec 
+         int n= sizeof(gec)/sizeof(gec[0]);
+    int min_value = gec[0];  // initialize min as 0
+        for(int i=0;i<n;i++){
+          if(gec[i]<min_value){
+             min_value=gec[i]; // update min value 
+        }
+    } 
+    cout<<" The smallest number is : "<< min_value << endl;
+   return 0;
+}
